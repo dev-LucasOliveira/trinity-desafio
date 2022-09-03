f73684b1f7d3e6a6ba1a0a79c8b39f69e52d275b
the-pi-user <105022212+the-pi-user@users.noreply.github.com>
2022-09-03 20:51:01 -0300

    modal funtional 1.0 (not totally responsive)

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const modalDiv = document.getElementById('modal-div');
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const button3 = document.getElementById('button3');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const role = document.getElementById('role');
+const birthday = document.getElementById('birthday');
+const statusHTML = document.getElementById('status');
+const mode = document.getElementById('mode');
+const image = document.getElementById('image');
 
 let showModal = false;
 
@@ -17,8 +24,58 @@ function toggleModal() {
     };
 }
 
-let indexButton = 1;
+let studentsArray = [
+    
+    {'image': 'https://i.im.ge/2022/09/03/ObJ276.OQPoT0-DSC-0368.jpg',
+    'firstName': 'Lucas', 
+    'lastName': 'Oliveira',
+    'role': 'Monitor',
+    'birthday': '2001/01/14',
+    'statusHTML': 'Active',
+    'mode': 'Onsite'},
 
+    {'image': 'https://i.im.ge/2022/09/03/ObJ276.OQPoT0-DSC-0368.jpg',
+    'firstName': 'Pedro', 
+    'lastName': 'Luna',
+    'role': 'Monitor',
+    'birthday': '2001/01/14',
+    'statusHTML': 'Active',
+    'mode': 'Onsite'},
+
+    {'image': 'https://i.im.ge/2022/09/03/ObJ276.OQPoT0-DSC-0368.jpg',
+    'firstName': 'Marlos', 
+    'lastName': 'Gonçalves',
+    'role': 'Monitor',
+    'birthday': '2001/01/14',
+    'statusHTML': 'Active',
+    'mode': 'Onsite'},
+
+]
+
+function submitCard() {
+
+    let student = {'image': image.value, 'firstName': firstName.value, 'lastName': lastName.value, 'birthday': birthday.value, 'role': role.value, 'statusHTML': statusHTML.value, 'mode': mode.value};
+    // console.log(student);
+
+    studentsArray.unshift(student);
+    console.log(studentsArray);
+
+    image.value = '';
+    firstName.value = '';
+    lastName.value = '';
+    birthday.value = '';
+    role.value = '';
+    statusHTML.value = '';
+    mode.value = '';
+    modalDiv.style.display = 'none';
+    cardBox.innerHTML = '';
+
+    studentsArray => studentsArray.splice(1,12);
+    studentsArray.forEach((user, contagem) => (renderUserCard(user)));
+
+};
+
+let indexButton = 1;
 
 button1.style.backgroundColor = '#ffffff';
 
@@ -29,10 +86,13 @@ function showCards(btnNumber) {
         button2.style.backgroundColor = '#c79051a1';
         button3.style.backgroundColor = '#c79051a1';
         cardBox.innerHTML = '';
-        const userData = fetch('https://dummyjson.com/users')
-        .then(dadosCrus => dadosCrus.json())
-        .then(dadosJson => dadosJson.users.splice(1,12)
-            .forEach((user, contagem) => renderUserCard(user)));
+
+
+        studentsArray.forEach((user, contagem) => {
+            studentsArray.splice(1,12)
+            renderUserCard(user)});
+
+
     }
     if (btnNumber === 2) {
         indexButton === 2;
@@ -40,10 +100,13 @@ function showCards(btnNumber) {
         button2.style.backgroundColor = '#ffffff';
         button3.style.backgroundColor = '#c79051a1';
         cardBox.innerHTML = '';
-        const userData = fetch('https://dummyjson.com/users')
-        .then(dadosCrus => dadosCrus.json())
-        .then(dadosJson => dadosJson.users.splice(11,12)
-            .forEach((user, contagem) => renderUserCard(user)));
+
+
+        studentsArray.forEach((user, contagem) => {
+            studentsArray.splice(11,12)
+            renderUserCard(user)});
+
+
     }
     if (btnNumber === 3) {
         indexButton === 3;
@@ -51,20 +114,23 @@ function showCards(btnNumber) {
         button2.style.backgroundColor = '#c79051a1';
         button3.style.backgroundColor = '#ffffff';
         cardBox.innerHTML = '';
-        const userData = fetch('https://dummyjson.com/users')
-        .then(dadosCrus => dadosCrus.json())
-        .then(dadosJson => dadosJson.users.splice(21,12)
-            .forEach((user, contagem) => renderUserCard(user)));
+
+        
+        studentsArray.forEach((user, contagem) => {
+            studentsArray.splice(21,12)
+            renderUserCard(user)});
+
+
     };
 }
 
-function renderUserCard(userData) {
+function renderUserCard(studentsArray) {
     const cardHTML = `
         <div class="card">      
-            <img class="image" src=${userData.image}>
+            <img class="image" src=${studentsArray.image}>
             <div class="text-box">
                 <div class="name">
-                    <b> ${userData.firstName} ${userData.lastName} </b>
+                    <b> ${studentsArray.firstName} ${studentsArray.lastName} </b>
                 </div>
                 <div class="description">
                     <b>Role:</b>
@@ -73,10 +139,10 @@ function renderUserCard(userData) {
                     <b>Mode:</b> 
                 </div>
                 <div class="personal-description">
-                    <div class="role"> Teacher </div>
-                    <div class="birthday"> ${userData.birthDate} </div>
-                    <div class="status"> Active </div>
-                    <div class="mode"> Onsite </div>                        
+                    <div class="role"> ${studentsArray.role} </div>
+                    <div class="birthday"> ${studentsArray.birthday} </div>
+                    <div class="status"> ${studentsArray.statusHTML} </div>
+                    <div class="mode"> ${studentsArray.mode} </div>                        
                 </div>  
             </div>
         </div>
@@ -86,7 +152,10 @@ function renderUserCard(userData) {
     cardBox.appendChild(newCard);
 };
 
-const userData = fetch('https://dummyjson.com/users')
+studentsArray => studentsArray.splice(1,12);
+studentsArray.forEach((user, contagem) => (renderUserCard(user)));
+
+/* const userData = fetch('https://dummyjson.com/users')
 .then(dadosCrus => dadosCrus.json())
 .then(dadosJson => dadosJson.users.splice(1,12)
-    .forEach((user, contagem) => renderUserCard(user)));
+    .forEach((user, contagem) => renderUserCard(user))); */
